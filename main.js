f07de5e3a0a6f46c938605d8bf3640b6a7e7747b
Hessen Nasser <hessenaloosh@gmail.com>
2023-02-24 02:20:25 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let nav = document .querySelector('nav');
+let dropMenu = document .querySelector('.drop-menu');
+let menuIcon = document .querySelector('.menu-icon');
+
+menuIcon.addEventListener("click", () => nav.classList.toggle('active'))
